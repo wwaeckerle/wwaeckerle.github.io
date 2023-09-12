cbff612d6e2c37dc80d4d5398ab55b7bbc75e75f
Whitney W <131135324+wwaeckerle@users.noreply.github.com>
2023-09-12 13:04:51 -0500

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,4 @@
-(function() {
-    let button = document.getElementById('toggle-menu')
-    button.addEventListener('click',function(event) {
-        event.preventDefault();
-        let menu = document.getElementById('main-menu');
-        menu.classList.toggle('is-open');
-    })
-    });
-
-    $('.smooth').on('click',function(){
-        $.smoothScroll({
-            scrollElement: $('body'),
-            scrollTarget: '#'+ this.id
-        });
-
-        return false;
-    });
+$("document").ready(function()  {
+  $("#Section_One").css("border", "3px solid #322E3D");
+         });
 
